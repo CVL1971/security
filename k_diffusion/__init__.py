d79c23fdcdff09ed65b83f440d4b3caced5a0299
CVL1971 <109881278+CVL1971@users.noreply.github.com>
2022-10-01 02:54:27 +0200

    Update __init__.py

--- k_diffusion/__init__.py
+++ k_diffusion/__init__.py
@@ -1,2 +1,2 @@
-from . import augmentation, config, evaluation, external, gns, layers, models, utils
+from . import augmentation, config, evaluation, external, gns, layers, models, sampling, utils
 from .layers import Denoiser
